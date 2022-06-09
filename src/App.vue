d76bc35050cc11d511c06d4203227f1e301577e5
kac-ershe <97762840+kyartscouncil@users.noreply.github.com>
2022-06-09 16:42:13 -0400

    Initial setup

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,21 @@
 <script setup>
-// This starter template is using Vue 3 <script setup> SFCs
-// Check out https://vuejs.org/api/sfc-script-setup.html#script-setup
-import HelloWorld from './components/HelloWorld.vue'
+	import TheHeader from './components/TheHeader.vue';
 </script>
 
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Hello Vue 3 + Vite" />
+	<TheHeader />
+	<div>
+		<h1>app.vue</h1>
+	</div>
 </template>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+	#app {
+		font-family: Avenir, Helvetica, Arial, sans-serif;
+		-webkit-font-smoothing: antialiased;
+		-moz-osx-font-smoothing: grayscale;
+		text-align: center;
+		color: #2c3e50;
+		margin-top: 60px;
+	}
 </style>
---
+++ src/pages/Grants.vue
@@ -0,0 +1,5 @@
+<template>
+	<div>
+		<h1>Grants</h1>
+	</div>
+</template>
